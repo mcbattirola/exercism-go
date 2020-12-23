8aa30b2c3c75973b90b5b49f66c2b15495653f13
mcbattirola <mcbattirola@hotmail.com>
2020-12-22 21:21:27 -0300

    Scrabble and approved raindrops.

--- raindrops/raindrops.go
+++ raindrops/raindrops.go
@@ -6,33 +6,28 @@ The rules of raindrops are that if a given number:
 has 3 as a factor, add 'Pling' to the result.
 has 5 as a factor, add 'Plang' to the result.
 has 7 as a factor, add 'Plong' to the result.
-does not have any of 3, 5, or 7 as a factor, the result should be the digits of the number.
-
-*/
+does not have any of 3, 5, or 7 as a factor, the result should be the digits of the number.*/
 package raindrops
 
-import "fmt"
+import "strconv"
 
 // Convert returns the raindrop sound string from input
 func Convert(input int) string {
 	raindrop := ""
-	if hasFactor(input, 3) {
+
+	if input%3 == 0 {
 		raindrop += "Pling"
 	}
-	if hasFactor(input, 5) {
+	if input%5 == 0 {
 		raindrop += "Plang"
 	}
-	if hasFactor(input, 7) {
+	if input%7 == 0 {
 		raindrop += "Plong"
 	}
 
 	if raindrop == "" {
-		raindrop = fmt.Sprint(input)
+		return strconv.Itoa(input)
 	}
 
 	return raindrop
-}
-
-func hasFactor(num int, factor int) bool {
-	return num%factor == 0
 }
---
+++ scrabble-score/scrabble_score.go
@@ -0,0 +1,49 @@
+// Package scrabble helps with scrabble computarions
+package scrabble
+
+import "strings"
+
+func Score(s string) int {
+	points := 0
+
+	sUpperCased := strings.ToUpper(s)
+	for _, c := range sUpperCased {
+		points += getRunePoints(c)
+	}
+
+	return points
+
+}
+
+func getRunePoints(c rune) int {
+	if strings.ContainsRune("AEIOULNRST", c) {
+		return 1
+	}
+
+	if strings.ContainsRune("DG", c) {
+		return 2
+	}
+
+	if strings.ContainsRune("BCMP", c) {
+		return 3
+	}
+
+	if strings.ContainsRune("FHVWY", c) {
+		return 4
+	}
+
+	if strings.ContainsRune("K", c) {
+		return 5
+	}
+
+	if strings.ContainsRune("JX", c) {
+		return 8
+	}
+
+	if strings.ContainsRune("QZ", c) {
+		return 10
+	}
+
+	return 0
+
+}
