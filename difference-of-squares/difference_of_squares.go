33b8028cf447fa69d887cdf0ee97cc6cf3317ea2
mcbattirola <mcbattirola@hotmail.com>
2021-01-10 09:50:27 -0300

    Difference of squares solved.

---
+++ difference-of-squares/difference_of_squares.go
@@ -0,0 +1,37 @@
+// Package diffsquares has functins to calculate the difference between the square of the sum and the sum of the squares of the first N natural numbers.
+package diffsquares
+
+// SquareOfSum calculates the square of the sum of the first n natural numbers.
+func SquareOfSum(n int) int {
+	if n <= 0 {
+		return 0
+	}
+
+	sum := 0
+
+	for i := 1; i <= n; i++ {
+		sum += i
+	}
+
+	return sum * sum
+}
+
+// SumOfSquares calculates the sum of the squares of the first n natural numbers.
+func SumOfSquares(n int) int {
+	if n <= 0 {
+		return 0
+	}
+
+	sumOfSquares := 0
+
+	for i := 1; i <= n; i++ {
+		sumOfSquares += i * i
+	}
+
+	return sumOfSquares
+}
+
+// Difference calculates the difference between the square of the sum and the sum of the squares of the first n natural numbers.
+func Difference(n int) int {
+	return SquareOfSum(n) - SumOfSquares(n)
+}
